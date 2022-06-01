80c114ba3681a6dc08c4b2b37c0460987d37df75
andriyansyah0003 <andriyansyah0003@gmail.com>
2022-06-02 00:39:38 +0700

    first version

--- index.js
+++ index.js
@@ -1,104 +1,132 @@
 const express = require("express");
 
 const app = express();
-const PORT = 3000;
+const port = 3000;
 
-const projects = [];
+const isLogin = true;
+let projects = [
+  {
+    projectName: "Personal Website",
+    startDate: "2022-06-01",
+    endDate: "2022-07-01",
+    lengthDate: "1 Months",
+    description:
+      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.",
+    nodeJs: "public/img/nodejs-icon.svg",
+    reactJs: "public/img/react.svg",
+    nextJs: "public/img/nextjs-icon.svg",
+    typeScript: "public/img/typescript-icon.svg",
+    projectImage: "img/cover.png",
+  },
+];
 
-app.set("view engine", "hbs"); // set the view engine to handlebars
+app.set("view engine", "hbs");
 
-app.use("/public", express.static(__dirname + "/public")); // set the public folder as static
-app.use(express.urlencoded({ extended: false })); // set the body parser
+app.use("/public", express.static(__dirname + "/public"));
+app.use(express.urlencoded({ extended: false }));
 
-app.get("/", (req, res) => {
-  res.render("index", { projects });
-});
-
-app.get("/add-project", (req, res) => {
-  res.render("add-project");
-
+app.get("/", function (req, res) {
   const newProject = projects.map((project) => {
-    projects.lengthDate = getDateDifference(
-      new Date(projects.start_date),
-      new Date(projects.end_date)
-    );
-    console.log(projects.lengthDate);
+    project.isLogin = isLogin;
+    return project;
   });
+  res.render("index", { isLogin: isLogin, projects });
 });
-app.post("/add-project", (req, res) => {
+
+app.get("/home", function (req, res) {
+  const newProject = projects.map((project) => {
+    project.isLogin = isLogin;
+    return project;
+  });
+
+  res.render("index", { isLogin: isLogin, projects });
+});
+
+app.get("/add-project", function (req, res) {
+  res.render("add-project");
+});
+
+app.post("/add-project", function (req, res) {
   const data = req.body;
 
   console.log(data);
+
+  data.lengthDate = getDateDifference(data["startDate"], data["endDate"]);
+  data["startDate"] = converTime(data["startDate"]);
+  data["endDate"] = converTime(data["endDate"]);
+
   projects.push(data);
+  res.redirect("/");
+});
+
+app.get("/project-detail/:index", function (req, res) {
+  const index = req.params.index;
+  const project = projects[index];
+
+  res.render("project-detail", { isLogin: isLogin, project });
+});
+
+app.get("/contact", function (req, res) {
+  res.render("contact", { isLogin: isLogin });
+});
+
+app.get("/edit-project/:index", function (req, res) {
+  const index = req.params.index;
+  const edit = projects[index];
+
+  res.render("edit-project", { isLogin: isLogin, edit, id: index });
+});
+
+app.post("/edit-project/:index", function (req, res) {
+  const data = req.body;
+  const index = req.params.index;
+
+  data.lengthDate = getDateDifference(data["startDate"], data["endDate"]);
+  data["start-date"] = converTime(data["startDate"]);
+  data["end-date"] = converTime(data["endDate"]);
+
+  console.log(data);
+
+  projects[index] = data;
 
   res.redirect("/");
 });
 
-app.get("/detail-project", (req, res) => {
-  res.render("project-detail", { projects });
+app.get("/delete-project/:index", (req, res) => {
+  const index = req.params.index;
+  projects.splice(index, 1);
+
+  res.redirect("/");
 });
 
-app.get("/contact", (req, res) => {
-  res.render("contact");
+app.listen(port, function () {
+  console.log(`Server running on port: ${port}`);
 });
-
-app.listen(PORT, () => {
-  console.log(`Server started on port ${PORT}`);
-});
-
-// function getFullTime(time) {
-//   const date = time.getDate();
-//   const monthIndex = time.getMonth();
-//   const year = time.getFullYear();
-//   let hour = time.getHours();
-//   let minute = time.getMinutes();
-
-//   if (hour < 10) {
-//     hour = "0" + hour;
-//   }
-
-//   if (minute < 10) {
-//     minute = "0" + minute;
-//   }
-
-//   const fullTime = `${date} ${month[monthIndex]} ${year} ${hour}:${minute} WIB`;
-
-//   return fullTime;
-// }
 
 function getDateDifference(startDate, endDate) {
-  // get date difference
-  if (startDate > endDate) {
-    // check if start date is greater than end date
-    console.error("Start date must be before end date"); // throw error
-    return null; // return null
-  }
-  const startYear = startDate.getFullYear(); // get start year
-  const startMonth = startDate.getMonth(); // get start month
-  const startDay = startDate.getDate(); // get start day
+  startDate = new Date(startDate);
+  endDate = new Date(endDate);
+  const startDateUTC = Date.UTC(
+    startDate.getFullYear(),
+    startDate.getMonth(),
+    startDate.getDate()
+  );
+  const endDateUTC = Date.UTC(
+    endDate.getFullYear(),
+    endDate.getMonth(),
+    endDate.getDate()
+  );
+  day = 1000 * 60 * 60 * 24; // miliseconds in a day
+  difference = (endDateUTC - startDateUTC) / day; // difference in days
+  return difference < 30
+    ? difference + " Days"
+    : parseInt(difference / 30) + " Months"; // return difference in months
+}
 
-  const endYear = endDate.getFullYear(); // get end year
-  const endMonth = endDate.getMonth(); // get end month
-  const endDay = endDate.getDate(); // get end day
-
-  const february =
-    (endYear % 4 == 0 && endYear % 100 != 0) || endYear % 400 == 0 ? 29 : 28; // check if end year is leap year
-  const daysOfMonth = [31, february, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]; // get days of month
-
-  const startDateNotPassedInEndYear =
-    endMonth < startMonth || (endMonth == startMonth && endDay < startDay); // check if start date is not passed in end year
-  const years = endYear - startYear - (startDateNotPassedInEndYear ? 1 : 0); // get years
-  const months =
-    (12 + endMonth - startMonth - (endDay < startDay ? 1 : 0)) % 12; // get months
-
-  const days =
-    startDay <= endDay
-      ? endDay - startDay
-      : daysOfMonth[(12 + endMonth - 1) % 12] - startDay + endDay; // get days
-
-  return {
-    years: years,
-    months: months,
-    days: days,
-  };
+function converTime(time) {
+  return new Date(time).toLocaleDateString("id-ID", {
+    year: "numeric",
+    month: "long",
+    day: "numeric",
+  });
 }
